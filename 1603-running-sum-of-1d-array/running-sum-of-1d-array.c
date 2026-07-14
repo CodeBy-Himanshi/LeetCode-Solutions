41954efea54801b66978d7be0f93fffadda142b0
Himanshi <h41634969@gmail.com>
2026-07-14 20:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (53.10%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* runningSum(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    if (numsSize == 0) 
+        return result;
+    result[0] = nums[0];
+    for (int i = 1; i < numsSize; i++)
+        result[i] = result[i - 1] + nums[i];
+    return result;
+}
