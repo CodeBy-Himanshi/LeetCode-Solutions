558023bf49a6fb00f518521d4971d83ab985d5b7
Himanshi <h41634969@gmail.com>
2026-07-19 20:06:29 +0530

    Time: 13 ms (67.23%) | Memory: 25.6 MB (19.76%) - LeetSync

---
+++ 148-sort-list/sort-list.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode{
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* merge(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode dummy;
+    struct ListNode* tail=&dummy;
+    dummy.next=NULL;
+    while(l1!=NULL && l2!=NULL){
+        if(l1->val<l2->val){
+            tail->next=l1;
+            l1=l1->next;
+        } 
+        else{
+            tail->next=l2;
+            l2=l2->next;
+        }
+        tail=tail->next;
+    }
+    tail->next=(l1!=NULL) ? l1 : l2;
+    return dummy.next;
+}
+struct ListNode* sortList(struct ListNode* head){
+    if(head == NULL || head->next == NULL)
+        return head;
+    struct ListNode* slow=head;
+    struct ListNode* fast=head;
+    struct ListNode* prev=NULL;
+    while(fast!=NULL && fast->next!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    prev->next=NULL;
+    struct ListNode* l1=sortList(head);
+    struct ListNode* l2=sortList(slow);
+    return merge(l1, l2);
+}
